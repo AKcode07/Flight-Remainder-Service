16c2aa0c21dc0f0bd419fbe4511d976f60e06dc8
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-14 13:18:41 +0530

    14th June 2024

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const setupAndStartServer = async () => {
 
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-        jobs();
+        // jobs();
         
     });
 }
