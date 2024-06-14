d9f1718aea90e7ce2631ec3f991dabb34464592e
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-14 16:39:20 +0530

    14th June 2024

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
 const TicketController = require('./controllers/ticket-controller');
-// const EmailService = require('./services/email-service');
+const EmailService = require('./services/email-service');
 
 const jobs = require('./utils/job');
-// const { subscribeMessage, createChannel } = require('./utils/messageQueue');
-// const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
     const app = express();
@@ -17,8 +17,8 @@ const setupAndStartServer = async () => {
 
     app.post('/api/v1/tickets', TicketController.create);
 
-    // const channel = await createChannel();
-    // subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
 
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
